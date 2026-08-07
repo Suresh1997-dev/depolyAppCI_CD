8b4140a31f91cb8ceb0767776dbf18b8a8f057ef
Suresh1997-dev <Suresh@nastaf.com>
2026-08-07 17:44:42 +0530

    first commit

---
+++ deployapp/src/component/url.js
@@ -0,0 +1,9 @@
+// export const Apiurl = "http://localhost:8082"
+
+// export const Apiurl = "https://kittytesting.onrender.com"
+// its dockeruse
+// export const Apiurl = "https://real-live-docker-2.onrender.com"
+
+// using nodejs
+
+export const Apiurl = "https://real-live-docker-2-n3lg.onrender.com"
